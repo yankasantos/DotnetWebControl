08e9296f442202013f7e41dd8ae7aeec7bde7013
yankasantos <yankasantos0107@gmail.com>
2019-11-19 09:53:20 -0300

    estrutura do projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotnetWepControl.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
